b41b4ac7873ae56057b65c52f0bb48eddbbc06e0
maricris800 <maricris800@hotmail.com>
2024-11-12 21:02:38 -0400

    avances sorpresa cumple

---
+++ script.js
@@ -0,0 +1,10 @@
+// Puedes agregar aquí funcionalidades interactivas, como:
+// - Cambiar el fondo al hacer clic en la imagen
+// - Mostrar un mensaje personalizado al pasar el ratón sobre la imagen
+// - Reproducir una canción de cumpleaños
+
+// Ejemplo simple:
+const imagen = document.querySelector('img');
+imagen.addEventListener('click', () => {
+    alert('¡Sorpresa! :-)');
+});
